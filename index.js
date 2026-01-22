dedf4e1512758b1c44bce516dafb07fc8f9f040f
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-01-22 23:32:49 +0530

    mongodb connect with nodes

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import { MongoClient } from 'mongodb';
+const app = express();
+app.set('view engine','ejs');
+
+const dbName = "school";
+const url = "mongodb://localhost:27017";
+const client = new MongoClient(url);
+
+
+app.get('/',async (req,res)=>{
+    await client.connect();
+    const db = client.db(dbName);
+    const collection = db.collection('student');
+    const result = await collection.find().toArray();
+    console.log(result);
+    res.render('student',{result});
+})
+
+app.listen(3200);
