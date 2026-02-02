9f9d26ae5a474f44bff06b055f41f4dca5680380
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-02-02 22:10:10 +0530

    connect mongoose

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import express from 'express';
-const app = app.express();
+import mongoose from 'mongoose';
+async function dbconnection(){
+  await mongoose.connect('mongodb://localhost:27017/school')
+  const schema = mongoose.Schema({
+    name: String,
+    age: Number,
+    email: String
+  })
+  const studentModel = mongoose.model('student', schema);
+  const result = await studentModel.find();
+  console.log(result);
+}
+dbconnection();
 
     
     
