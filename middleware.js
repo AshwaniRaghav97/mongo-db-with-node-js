08b6b6bb20c4035ff0de4abced995a9f7a246cee
Ashwani Raghav <ashwani.raghav_cs23@gla.ac.in>
2026-04-08 10:09:43 +0530

    committed

--- middleware.js
+++ middleware.js
@@ -1,12 +1,24 @@
-let express = require('express');
+import express from "express";
 let app = express();
 
 let router = express.Router();
 
 
 
+router.use((req,res,next)=>{
+    console.log('Time:', Date.now());
+    next();
+})
 
 
+router.use('/user/:id',(req,res,next)=>{
+    console.log("route", req.method);
+    next();
+},(req,res,next)=>{
+  console.log("method", req.method);
+  next();
+}
+)
 
 app.listen(3000, () => {
     console.log('Example app listening on port 3000!');
